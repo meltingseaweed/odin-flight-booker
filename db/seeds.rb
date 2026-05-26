f0b705aa1e99998f386ce09c057a22c691499c1a
meltingseaweed <conor.seo@gmail.com>
2026-05-26 11:54:17 +0900

    Feat: Populate seeds with flights, airports, and link together

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,14 +19,10 @@ end
 
 
 airports = [
-  { code: 'VIE', city: 'Vienna' },
   { code: 'BRU', city: 'Brussels' },
-  { code: 'CPH', city: 'Copenhagen' },
   { code: 'AKJ', city: 'Asahikawa' },
   { code: 'HEL', city: 'Helsinki' },
   { code: 'CDG', city: 'Paris (Charles de Gaulle)' },
-  { code: 'OSL', city: 'Oslo' },
-  { code: 'ATH', city: 'Athens' },
   { code: 'DUB', city: 'Dublin' }
 ]
 
@@ -34,53 +30,57 @@ airports.each do |airport|
   Airport.create!(code: airport[:code], city: airport[:city])
 end
 
-vie = Airport.find_by(code: 'VIE')
 bru = Airport.find_by(code: 'BRU')
-cph = Airport.find_by(code: 'CPH')
 akj = Airport.find_by(code: 'AKJ')
 hel = Airport.find_by(code: 'HEL')
 cdg = Airport.find_by(code: 'CDG')
-osl = Airport.find_by(code: 'OSL')
-ath = Airport.find_by(code: 'ATH')
 dub = Airport.find_by(code: 'DUB')
 
-# vie.Flight.create!(
-#   departure_airport: vie,
-#   arrival_airport: bru,
-#   start: DateTime.new(2026, 6, 1, 10, 0, 0),
-#   duration: 105 # In minutes
-# )
 
-# Flight.create!(
-# departure_airport: cdg,
-# arrival_airport: akj,
-# start: DateTime.new(2025, 6, 7, 13, 30, 0),
-# duration: 800
-# )
+f1 = Flight.create!(
+  departure_airport: hel.code,
+  arrival_airport: bru.code,
+  start: DateTime.new(2026, 6, 1, 10, 0, 0),
+  duration: 155
+)
 
-# Flight.create!(
-#   departure_airport: cph,
-#   arrival_airport: dub,
-#   start: DateTime.new(2026, 6, 3, 10, 15, 0),
-#   duration: 140
-# )
-# Flight.create!(
-#   departure_airport: ath,
-#   arrival_airport: hel,
-#   start: DateTime.new(2026, 6, 1, 13, 0, 0),
-#   duration: 105 # In minutes
-# )
+f2 = Flight.create!(
+  departure_airport: bru.code,
+  arrival_airport: hel.code,
+  start: DateTime.new(2026, 6, 1, 15, 0, 0),
+  duration: 155
+)
 
-# Flight.create!(
-#   departure_airport: bru,
-#   arrival_airport: osl,
-#   start: DateTime.new(2026, 6, 4, 11, 30, 0),
-#   duration: 800
-# )
 
-# Flight.create!(
-#   departure_airport: dub,
-#   arrival_airport: akj,
-#   start: DateTime.new(2026, 6, 2, 8, 15, 0),
-#   duration: 840
-# )
+f3 = Flight.create!(
+departure_airport: cdg.code,
+arrival_airport: akj.code,
+start: DateTime.new(2025, 6, 18, 13, 30, 0),
+duration: 800
+)
+
+f4 = Flight.create!(
+departure_airport: akj.code,
+arrival_airport: cdg.code,
+start: DateTime.new(2025, 6, 7, 13, 30, 0),
+duration: 800
+)
+
+f5 = Flight.create!(
+departure_airport: bru.code,
+arrival_airport: dub.code,
+start: DateTime.new(2025, 6, 7, 13, 30, 0),
+duration: 95
+)
+
+f6 = Flight.create!(
+departure_airport: dub.code,
+arrival_airport: bru.code,
+start: DateTime.new(2025, 6, 7, 13, 30, 0),
+duration: 95
+)
+
+bru.schedules.create!(departing_flights: f2)
+bru.schedules.create!(departing_flights: f5)
+bru.arrivals.create!(arriving_flights: f1)
+bru.arrivals.create!(arriving_flights: f6)
