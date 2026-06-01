6930d5d6e670a9b71feca8cb183465d29b30b0e4
meltingseaweed <conor.seo@gmail.com>
2026-06-01 14:29:20 +0900

    Feat: Add dependent destroy
    Add dependent destroy to associations and similar
    for nested associations

--- app/models/airport.rb
+++ app/models/airport.rb
@@ -1,6 +1,6 @@
 class Airport < ApplicationRecord
-  has_many :schedules, class_name: "Schedule", foreign_key: :departing_airport_id
-  has_many :arrivals, class_name: "Arrival", foreign_key: :arriving_airport_id
+  has_many :schedules, class_name: "Schedule", foreign_key: :departing_airport_id, dependent: :destroy
+  has_many :arrivals, class_name: "Arrival", foreign_key: :arriving_airport_id, dependent: :destroy
 
   has_many :departing_flights, through: :schedules
   has_many :arriving_flights, through: :arrivals
--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,9 +1,9 @@
 class Flight < ApplicationRecord
-  has_many :schedules, class_name: "Schedule", foreign_key: :departing_flights_id
-  has_many :arrivals, class_name: "Arrival", foreign_key: :arriving_flights_id
+  has_many :schedules, class_name: "Schedule", foreign_key: :departing_flights_id, dependent: :destroy
+  has_many :arrivals, class_name: "Arrival", foreign_key: :arriving_flights_id, dependent: :destroy
 
   has_many :departing_airports, through: :schedules
   has_many :arriving_airports, through: :arrivals
 
-  has_many :bookings
+  has_many :bookings, dependent: :destroy
 end
